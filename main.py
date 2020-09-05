07f49864d4bd1bad2a2a7d66755ec3a1b5dcc63b
Gabulhas <guilhermebidarra@gmail.com>
2020-09-05 19:11:50 +0100

    added hosts and port to env

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, render_template
 from home.views import home
 from dashboard.views import dashboard
 import settings
+import os
 
 settings.load_config()
 
@@ -9,9 +10,11 @@ app = Flask(__name__)
 app.register_blueprint(home, url_prefix="/home")
 app.register_blueprint(dashboard, url_prefix="/dashboard")
 
+
 @app.route("/")
 def root():
     return redirect(url_for("home.home_index"))
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(os.getenv("HOST"), port=os.getenv("PORT"))
